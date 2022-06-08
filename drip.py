414f2a0602cdea0bca32b362f917ac849d670094
gferrari18 <gferrari18@my.whitworth.edu>
2022-06-07 20:06:54 -0700

    New Drip

---
+++ drip.py
@@ -0,0 +1,21 @@
+class Customers:
+    def __init__(self, ID=0, Company="", LastName="",FirstName="", email="", Address="", JobTitle="", Business="", Phone=0, HomePhone=0, Mobile=0, FaxNumber=0,City="",StateProvince="",zip=0,Country="",WebPage="",Notes="",Attach=""):
+        self.ID = ID
+        self.Company = Company
+        self.LastName = LastName
+        self.FirstName = FirstName
+        self.email = email
+        self.Address = Address
+        self.JobTitle = JobTitle
+        self.Business = Business
+        self.phone = Phone
+        self.HomePhone = HomePhone
+        self.Mobile= Mobile
+        self.FaxNumber = FaxNumber
+        self.City = City
+        self.StateProvince = StateProvince
+        self.zip = zip
+        self.Country = Country
+        self.WebPage = WebPage
+        self.Notes = Notes
+        self.Attach = Attach
